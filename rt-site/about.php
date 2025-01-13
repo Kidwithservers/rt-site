31a141c3ef252a3b69807bd2956c45509479e5b0
Enzo Damato <edamato@ctc.sh>
2025-01-13 15:55:32 -0600

    started rt site

---
+++ rt-site/about.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/about.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="fonts/helvetica.css">
+        <title>OWLink Communications</title>
+    </head>
+    <body>
+        <?php include "html/header.html"; ?>
+        <div class="about-body">
+            <h1><span>About</span>Us</h1>
+            <P>
+                At OWLink, we are a team of dedicated, forward thinking innovators
+                and telecoms nerds, passionate about telecommunications industry, 
+                and our customers, committed to building top-shelf, standards compliant 
+                products and services, that respect the intelligence and common sense of our end-users.
+                <br>
+                Unlike many other providers, we are deeply committed being good citizens
+                of the telecoms community, and strive for over-and-above regulatory compliance
+                and abuse prevention. We implement STIR/SHAKEN robocall mitigation, in addition
+                to next-generation internal statistical algorithms to detect and prevent network
+                abuse by robocallers and scammers. We are also go above and beyond to support
+                all registered carrier requirements, from standard 499/USF requirements to
+                those requirements typically reserved for ILECs exclusively, such as LD equal access.
+                <br>
+                We are a full-facilities based CLEC, with direct
+                control over all of our own numbering resources, in addition to a direct
+                connection with the NPAC, which means that we are in total control of our
+                numbering resources, ensuring accurate completion of port-in and out requests,
+                in addition to the ability to assign as many contiguous numbers as needed.
+                <br>
+                We also custom-build all of our telephone switches, rather than simply reselling
+                service from another carrier, or using a standard, off-the-shelf switching platform.
+                Because of this, we are able to offer advanced telephone features, and fully control
+                the flow of calls through our network. This custom switch implements a unique
+                routing structure, enabling our unique unified communications products.
+                <br>
+                Because we control both our own numbers and our own switches, we are able to
+                directly control our own routing, with direct local interconnection agreements
+                and direct agreements with FG-D tandem providers. We build LERG entries for
+                all of our number blocks and LRNs with our own CLLIs. We don't rely on one of
+                the big two intermediary companies, but instead rely on our own infrastructure
+                as much as possible, and use a diverse network of reputable, tier-one providers
+                to ensure maximal uptime.
+                <br>
+                Finally, we take an active role in the telecoms regulatory space, contributing
+                to FCC and state PUC requests for comment, rulemaking proceedings and hearings.
+                We are also actively working on pushing forward the state of telecommunications
+                regulations in the US. From pushing for better regulations on robocalling to
+                standardizing SIP over IP interconnection for local interconnects, we pride
+                ourselves for being on the forefront of the regulatory landscape.
+                <br>
+                Put together, we're a technologically innovative company, on the cutting-edge
+                of the telecommunications landscape, 
+            </p>
+        </div>
+        <?php include "html/footer.html"; ?>
+    </body>
+</html>
