f8737c6f1f497e1dcd7ef1035a2aa01b76cccbc1
Enzo Damato <edamato@ctc.sh>
2025-01-14 07:46:08 -0600

    Started copy and design for RT site

--- rt-site/index.php
+++ rt-site/index.php
@@ -16,146 +16,27 @@
                 <source src="img/wave-vid.mp4" type="video/mp4" />
             </video>
             <div class="caption">
-                <h1>Innovative <span>Telecommunications</span> Solutions for the Most Demanding Communications Needs</h1>
+                <h1>Your telecom problems: <span>Solved.</span></h1>
             </div>
         </div>
         <div class="fp-homeoverview">
-            <h2>Services</h2>
-            <div class="fp-ovbox">
-                <h3>Basic Landline</h3>
-                <p>
-                    Looking for a simple, affordable, phone line for your home or business? 
-                    Whether you are looking to set up a new phone, or you are looking to 
-                    replace your overpriced, spam-riddled traditional copper landline, our landline
-                    service will allow your to keep your existing phone, while receiving all the 
-                    benefits of next-generation VoIP service.
-                </p>
-            </div>
-            <div class="fp-ovbox">
-                <h3>Virtual Phone Numbers</h3>
-                <p>
-                    Tired of giving out your real phone number to spammers? Need an additional number
-                    for work calls? Our virtual phone number service will allow you to set up additional
-                    phone numbers which you can configure to route to your main line, your voicemail,
-                    SMS, email, a pager, or any other destination. These numbers can be configured to 
-                    route based on caller, spam status, time of day, or based on an IVR system.
-                </p>
-            </div>
-            <div class="fp-ovbox">
-                <h3>Unity Phone</h3>
-                <p>
-                    Our unity phone system unifies your home/business and mobile communications,
-                    allowing you to take your phone number and communications with you wherever you
-                    go. Unity phone combines the best features of a next-generation IP phone system with the
-                    mobility and SMS features of a cell phone, making it the best choice for those 
-                    looking to modernize their personal communications.
-                </p>
-            </div>
-            <div class="fp-ovbox">
-                <h3>Fax Service</h3>
-                <p>
-                    Need to fax? Our fax service allows you to send and receive 
-                    high-quality faxes over a traditional physical fax machine or over email. 
-                    Available as either a standalone service, or as an add-on to any existing phone number.
-                </p>
-            </div>
-        </div>
-        <div class="fp-advsvc">
-            <h2>Included Features</h2>
-            <div class="fp-svcbox">
-                <h3>Robocop advanced robocall mitigation</h3>
-                <p>
-                    All of our services include our Robocop anti-robocalling system. Robocop
-                    combines aggregated robocall data from tens of thousands of specially seeded
-                    trap numbers, complaints from real customers, network and signalling data, and
-                    realtime, interactive screening to deliver a highly effective and minimally intrusive
-                    screening experience. Even better than this, Robocop is adaptive, learning from
-                    every call it receives to automatically whitelist your contacts. In less than a
-                    week, Robocop will be able to block 99% of inbound robocalls, without annoying
-                    and intrusive voice prompts for every caller.
-                </p>
-            </div>
-            <div class="fp-svcbox">
-                <h3>SMS Service</h3>
-                <p>
-                    For residential customers of our unity phone service, we offer SMS service,
-                    accessible from any device connected to your number. As with our voice service,
-                    our SMS service includes latest-and-greatest spam filtering technology.
-                </p>
-            </div>
-            <div class="fp-svcbox">
-                <h3>Secure, encrypted calls</h3>
-                <p>
-                    All of our telephone switches support encrypted SIP and RTP streams, ensuring that
-                    your calls are secured against hackers, illegal wiretapping and ISP snooping.
-                </p>
-            </div>
-            <div class="fp-svcbox">
-                <h3>Caller Name</h3>
-                <p>
-                    We preform caller name (CNAM) lookups for all inbound calls, and refresh our
-                    internal databases weekly, ensuing that your phone always displays an accurate
-                    name for inbound callers. We also support syncing your contacts, ensuring accurate
-                    caller name for your home and office phones.
-                </p>
-            </div>
-            <div class="fp-svcbox">
-                <h3>Advanced voicemail</h3>
-                <p>
-                    All voicemail boxes support voicemail-to-email or SMS and optional voicemail
-                    transcription services, and large voicemail box size limits.
-                </p>
-            </div>
-            <div class="fp-svcbox">
-                <h3>Call forwarding</h3>
-                <p>
-                    All numbers support optional call forwarding for non-answered and 
-                    busy calls.
-                </p>
-            </div>
-            <div class="fp-svcbox">
-                <h3>Call waiting</h3>
-                <p>
-                    All of our lines support multiple simultaneous calls,
-                    enabling call waiting on supported hardware, and allow multiple
-                    users to make calls at the same time. 
-                </p>
-            </div>
-        </div>
-        <div class="fp-whyus">
-            <h2>Why OWLink?</h2>
-            <p>
-                We know the telecommunications space is highly competitive,
-                and that you likely have dozens of providers to choose from.
-                So, why use us? There's one clear reason: We Care. Our passion
-                for the industry and serving our customers is visible in everything
-                we do. 
-                <br><br>
-                It's why unlike almost all other carriers (including some
-                of the biggest names in the industry) we're full Local Exchange
-                Carriers, with our own direct connections to the PSTN. We aren't
-                an IP-only carrier relying on another company to host our numbers,
-                and we aren't an unregulated, fly-by-night reseller, simply billing
-                for access to other carrier's resources.
-                <br><br>
-                It's why we don't charge misleading and superfluous fees and surcharges,
-                or offer misleading "taxes and fees" plans, which camouflage the real cost
-                of the service in nebulous "subscriber line charge" fees.
-                <br><br>
-                It's why we are committed to ongoing innovation in the telecoms space,
-                innovating new features and services for our customers, helping to move
-                the telecommunications industry into the 21st century.
+            <h2>Solutions to your toughest Telecom Challenges</h2>
+            <P>
+                No matter what challenges you may be facing, 
             </p>
         </div>
-        <div class="fp-contactus">
-            <h2>Ready to upgrade your phone service? Contact us today!</h2>
-            <P>Contact us anytime to get set up, or to discuss your needs. We are 
-                passionate about telecoms, and are happy to discuss your needs. Weather
-                you need standard telephony service, or need a specialized, custom service,
-                we are always ready to help.
-            </P>
-            <p>E-Mail: contact@owlink.net <br>
-               Phone: (888) 555 - 777</p>
+        <div class="fp-homeoverview">
+            <h2>Advanced Robocall Intelligence</h2>
+            <P>
+                Our Robocop anti-robocall system gathers live robocall information from
+                tens of thousands of telephone numbers, using AI to monitor and
+                categorize all incoming calls, identifying key information about the caller
+                and carrier originating the call. This information is then analyzed to produce
+                real-time blacklists and drive interactive filtering systems.
+            </p>
+            <h3>Real-time robocall blacklist</h3>
+            <h3>Interactive Spam call screen</h3>
+            <h3>Robocall information database</h3>
         </div>
         <?php include "html/footer.html"; ?>
     </body>
